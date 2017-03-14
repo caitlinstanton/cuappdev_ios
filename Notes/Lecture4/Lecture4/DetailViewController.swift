50b7bcdec3942fd0b00d2ef4c6f729374f5a7a0b
Caitlin Stanton <caitlinstanton44@gmail.com>
2017-03-13 20:40:16 -0400

    lecture 4

--- Notes/Lecture4/Lecture4/DetailViewController.swift
+++ Notes/Lecture4/Lecture4/DetailViewController.swift
@@ -11,11 +11,28 @@ import UIKit
 class DetailViewController: UIViewController {
 
     var product: Product!
+    var imageView: UIImageView!
+    var sellerLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .white
+        
+        addUIElements()
+    }
+    
+    func addUIElements() {
+        imageView = UIImageView(frame: CGRect(x:0, y:0, width:view.frame.width, height:view.frame.height/2.0))
+        imageView.image = product.image
+        
+        sellerLabel = UILabel(frame: CGRect(x:0, y:imageView.frame.height+imageView.frame.origin.y+10, width:view.frame.width/2.0, height:30))
+        sellerLabel.center = CGPoint(x:view.center.x, y: sellerLabel.center.y)
+        sellerLabel.textAlignment = .center
+        sellerLabel.text = product.seller
+        
+        view.addSubview(imageView)
+        view.addSubview(sellerLabel)
     }
 
 }
